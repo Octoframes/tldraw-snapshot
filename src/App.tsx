e0d6cb125b53e62018da900971b69de06517cce7
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-11-07 12:05:26 +0100

    add tldraw

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Tldraw, useEditor } from "@tldraw/tldraw";
+import "@tldraw/tldraw/tldraw.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      style={{
+        position: "absolute",
+        width: 500,
+        height: 500,
+        top: 0,
+        left: 0,
+      }}
+    >
+      <Tldraw />
+      <SaveButton />
+    </div>
+  );
 }
 
-export default App
+function SaveButton() {
+  const editor = useEditor();
+  return (
+    <button
+      onClick={() => {
+        const snapshot = editor.store.getSnapshot();
+        const stringified = JSON.stringify(snapshot);
+        localStorage.setItem("my-editor-snapshot", stringified);
+      }}
+    >
+      Save
+    </button>
+  );
+}
