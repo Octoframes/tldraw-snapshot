aba8c827ac077ffc8789e78905db139004cd9ceb
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-11-08 10:58:33 +0100

    add text aread

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,48 @@
 import { Tldraw, useEditor } from "@tldraw/tldraw";
 import "@tldraw/tldraw/tldraw.css";
+import { useState } from "react";
 
 export default function App() {
+  const [snapshotData, setSnapshotData] = useState("");
+
   return (
     <div
       style={{
-        position: "absolute",
-        width: 500,
-        height: 500,
-        top: 0,
-        left: 0,
+        display: "flex",
+        position: "relative",
+        width: "100vw",
+        height: "100vh",
       }}
     >
-      <Tldraw>
-        <SaveButton />
-      </Tldraw>
+      <div style={{ width: 500, height: 500 }}>
+        <Tldraw>
+          <SaveButton onSave={setSnapshotData} />
+        </Tldraw>
+      </div>
+      <div style={{ marginLeft: 20, flex: 1 }}>
+        <textarea
+          style={{ width: "100%", height: 500 }}
+          value={snapshotData}
+          readOnly
+        />
+      </div>
     </div>
   );
 }
 
-function SaveButton() {
+function SaveButton({ onSave }) {
   const editor = useEditor();
 
   return (
     <button
-      style={{ position: "relative", zIndex: 1000 , left: 400, top: 0, backgroundColor: "lightblue" }}
+      style={{ position: "absolute", zIndex: 1000, right: 10, top: 10, backgroundColor: "lightblue" }}
       onClick={() => {
         const snapshot = editor.store.getSnapshot();
         const stringified = JSON.stringify(snapshot);
         localStorage.setItem("my-editor-snapshot", stringified);
         console.log("Saved!");
         console.log(stringified);
+        onSave(stringified);
       }}
     >
       Save
